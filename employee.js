e5b3e8c53b3116a3861765c1e530530ea65cca14
Nityam-07 <164447944+Nityam-07@users.noreply.github.com>
2024-08-13 13:39:39 +0530

    Update employee.js

--- employee.js
+++ employee.js
@@ -9,5 +9,29 @@ class Employee {
         return `Employee{name='${this.employeeName}', department='${this.department}', netPay=${this.netPay}}`;
     }
 }
+
+class Payroll {
+    constructor() {
+        this.employeeList = [];
+    }
  
-module.exports = Employee;
+    addEmployee(employee) {
+        this.employeeList.push(employee);
+    }
+ 
+    printEmployees() {
+        this.employeeList.forEach(employee => {
+            console.log(employee.toString());
+        });
+    }
+}
+
+const payroll = new Payroll();
+ 
+const emp1 = new Employee("John Doe", "IT", 75000);
+const emp2 = new Employee("Jane Smith", "HR", 65000);
+ 
+payroll.addEmployee(emp1);
+payroll.addEmployee(emp2);
+ 
+payroll.printEmployees();
